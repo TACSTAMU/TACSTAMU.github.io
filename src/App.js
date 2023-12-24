5d6dda1a4981bc67af01c7ce10160694132a006f
yoosh2842 <thereqltimel@gmail.com>
2023-12-24 17:02:20 -0600

    updated to react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+import test2 from './test2.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
