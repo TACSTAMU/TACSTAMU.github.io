d903674496295899724c214f9ae9bec2f787678d
yoosh2842 <thereqltimel@gmail.com>
2024-02-12 23:40:27 -0600

    no change

--- src/Volunteer.js
+++ src/Volunteer.js
@@ -2,7 +2,7 @@ const Volunteer = () => {
     return (
       <div>
         {/* jQuery first*/}
-        {/* Global site tag (gtag.js) - Google Analytics */}
+        {/* Global site tag (gtag.js) - Google Analytics  */}
         <meta charSet="utf-8" />
         <meta http-equiv="Refresh" content="0; url=https://tally.so/r/mVJXv6" /> 
         </div>
